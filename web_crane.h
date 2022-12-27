7397b3da1d2a02eb8fa3ff749d96bf40b62bca45
Jonas Nebl <jonas.nebl@t-online.de>
2022-12-28 00:39:13 +0100

    other files

---
+++ web_crane.h
@@ -0,0 +1,11 @@
+#ifndef WEB_CRANE_H_INCLUDED
+#define WEB_CRANE_H_INCLUDED
+
+void moveHaken(float[3]);
+float getLaenge(SEIL);
+void setAufhaengung(AUFHAENGUNG, AUFHAENGUNG, float[3], int);
+
+float skprodukt(float[3], float[3]);
+float norm(float[3]);
+
+#endif // WEB_CRANE_H_INCLUDED
