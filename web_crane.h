b915fea19f1de0742706711845d59fbd64f1743c
Jonas Nebl <jonas.nebl@t-online.de>
2022-12-30 00:17:16 +0100

    translated .h file to english

--- web_crane.h
+++ web_crane.h
@@ -10,31 +10,31 @@
 #include <math.h>
 #include <string.h>
 
-#define maxAnzahlAufhaengungen 5
+#define maxNumberPulleys 5
 
-const float radius = 5;
-const float LaengeAufhaengung = 20;
+const float r_pulley = 50;
+const float L_pulley = 150;
 
 typedef struct
 {
-    float laenge;
+    float length;
     int end_index;
-    float aufhaengungen[maxAnzahlAufhaengungen][4];
-}SEIL;
+    float pulleys[maxNumberPulleys][4];
+}CABLE;
 
-float skprodukt(float vector1[3], float vector2[3])
+float dotprodukt(float vector1[3], float vector2[3])
 {
-    float skalarprodukt = 0;
+    float dotproduct = 0;
     for (int i = 0; i<= 2; i++)
     {
-        skalarprodukt += vector1[i] * vector2[i];
+        dotproduct += vector1[i] * vector2[i];
     }
-    return skalarprodukt;
+    return dotproduct;
 }
 
 float norm(float vector[3])
 {
-    return sqrt(skprodukt(vector, vector));
+    return sqrt(dotproduct(vector, vector));
 }
 
 void copyarray(float *vector1, float *vector2)
@@ -45,101 +45,108 @@ void copyarray(float *vector1, float *vector2)
     }
 }
 
-float getLaenge(SEIL seil)
+float getLength(CABLE cable)
 {
-    float angles[maxAnzahlAufhaengungen - 2];
-    float Schnittpunkte[maxAnzahlAufhaengungen][3];
-    //Der erste Schnittpunkt ist gleich der Position der ersten Aufhängung
+    float angles[maxNumberPulleys - 2];
+    float intersections[maxNumberPulleysn][3];
+    //The first intersection equals the first pulley
     for (int j = 0; j<=2 ; j++)
     {
-        Schnittpunkte[0][j] = seil.aufhaengungen[0][j];
+        Schnittpunkte[0][j] = cable.pulleys[0][j];
     }
 
-    //Schrittweise Auffüllen der Schnittpunkte-Matrix
-    for (int i = 1; i <= seil.end_index - 1; i++)
+    // step by step filling up of intersections array
+    for (int i = 1; i <= cable.end_index - 1; i++)
     {
-
-        //Erzeugen zweier Vektoren, welche die drei Positionen verbinden
+        //Generating two vectors connecting three positions
         float vector1[3] = {0};
         float vector2[3] = {0};
         for (int j = 0; j<=2; j++)
         {
-            vector1[j] =  seil.aufhaengungen[i - 1][j] - seil.aufhaengungen[i][j];
-            vector2[j] =  seil.aufhaengungen[i + 1][j] - seil.aufhaengungen[i][j];
+            vector1[j] =  cable.pulleys[i - 1][j] - cable.pulleys[i][j];
+            vector2[j] =  cable.pulleys[i + 1][j] - cable.pulleys[i][j];
         }
 
-        //Berechnen des Winkels zwischen den beiden Vektoren
+        //Calculating angle between the vectors
         float angle = acos( skprodukt(vector1, vector2) / (norm(vector1)*norm(vector2)) );
         angles[i - 1] = angle; //Zwischenspeichern des Winkels für spätere Berechnung
 
-        //Berechnen des Schnittpunkts
+        //Calculating intersection
         float mvector[3] = {0};
         for (int j = 0; j<=2; j++)
         {
             mvector[j] = vector1[j] + vector2[j];
-        } //Erzeugen des Mittenvektors von vector1 und vector2
+        } //Calculating mean vector of vector1 and vector2
         float mvectorLength = norm(mvector);
         for (int j = 0; j<=2; j++)
         {
             mvector[j] /= mvectorLength;
-        } //Normieren des Mittenvektors
+        } //Normalizing mean vector
 
-        //Berechnenen des Abstands des Schnittpunkts und der Aufhängungsposition
+        //Calculating distance between intersection and mounting position of pulley
         float differenzlaenge = radius / sin(angle / 2) - LaengeAufhaengung;
         for (int j = 0; j<=2; j++)
         {
-            Schnittpunkte[i][j] = seil.aufhaengungen[i][j] - mvector[j] * differenzlaenge;
-        } //finales Ausrechnen des Schnittpunkts durch Verschieben der Aufhängungsposition entlang des Mittenvektors
+            Schnittpunkte[i][j] = cable.pulleys[i][j] - mvector[j] * differenzlaenge;
+        } // final calculating of intersection by shifting the mounting position along the mean vector
+        
     }
-    //Setzen des letzten Schnittpunkts in die Position der letzten Aufhängung
+    //Setting the latest intersection in the position of the last pulley
     for (int j = 0; j<=2 ; j++)
     {
-        Schnittpunkte[seil.end_index][j] = seil.aufhaengungen[seil.end_index][j];
+        Schnittpunkte[cable.end_index][j] = cable.pulleys[cable.end_index][j];
     }
 
-    //Nun wird die Seillänge berechnet
+    //Calculating length of the cable
     float laenge = 0;
-    for (int i = 0; i<= seil.end_index - 1; i++)
+    for (int i = 0; i<= cable.end_index - 1; i++)
     {
         float vector[3] = {0};
         for (int j = 0; j<=2; j++)
         {
             vector[j] = Schnittpunkte[i][j] - Schnittpunkte[i + 1][j];
-        } //Berechnend er Verbindungsvektoren der Sschnittpunkte
+        } //Calculating connecting vectors between intersections
 
         laenge += norm(vector);
 
-        if (i != seil.end_index - 1)
+        if (i != cable.end_index - 1)
         {
             laenge += radius * (3.14159 - angles[i] - 2 / tan( angles[i]/2 ));
-        } //Korrigieren der Rundung der Rolle mit einem Korrekturterm
+        } //Correction of the rounding of the pulley wheels with a correction term
     }
 
     return laenge;
 }
 
-void moveHaken(SEIL *seil, float x, float y, float z)
+
+
+void moveHaken(CABLE *cable, float x, float y, float z)
 {
-    //Die Funktion läuft die Aufhängungen-Matrix durch
-    for (int j = 0; j <= seil->end_index; j++)
+    //iterating through the pulleys array
+    for (int j = 0; j <= cable->end_index; j++)
     {
-    //Wenn die Matrix in der 4. Spalte eine 1 hat, wird die Aufhängung verschoben
-        if (  seil->aufhaengungen[j][3] )
+    //if the array has a 1 in the forth array, the pulley belongs to the hook and gets moved
+        if (  cable->pulleys[j][3] )
         {
-            seil->aufhaengungen[j][0] += x;
-            seil->aufhaengungen[j][1] += y;
-            seil->aufhaengungen[j][2] += z;
+            cable->pulleys[j][0] += x;
+            cable->pulleys[j][1] += y;
+            cable->pulleys[j][2] += z;
         }
     }
 }
 
-void setAufhaengung(SEIL *seil, float x, float y, float z, int Haken)
+void Simulink_moveHaken()
 {
-    seil->end_index++;
-    seil->aufhaengungen[seil->end_index][0] = x;
-    seil->aufhaengungen[seil->end_index][1] = y;
-    seil->aufhaengungen[seil->end_index][2] = z;
-    seil->aufhaengungen[seil->end_index][3] = Haken;
+
+}
+
+void setPulley(CABLE *cable, float x, float y, float z, int Haken)
+{
+    cable->end_index++;
+    cable->pulleys[cable->end_index][0] = x;
+    cable->pulleys[cable->end_index][1] = y;
+    cable->pulleys[cable->end_index][2] = z;
+    cable->pulleys[cable->end_index][3] = Haken;
 }
 
 #endif // WEB_CRANE_H_INCLUDED
